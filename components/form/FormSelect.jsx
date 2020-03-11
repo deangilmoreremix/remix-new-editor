a16ff8f2e07150e964e8e62ce8c6b3573118160d
aleksey leichenko <alekseyleychenko@gmail.com>
2020-03-11 13:33:43 +0200

    #1163890583803261 - Create select component

---
+++ components/form/FormSelect.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+import PropTypes from '../../lib/PropTypes';
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+function FormSelect(props) {
+  const { items, label, onChange, valueState } = props;
+  const classes = useStyles();
+
+  const inputLabel = useRef(null);
+  const [labelWidth, setLabelWidth] = useState(0);
+  useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+
+  const handleChange = event => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <div className="form-container">
+      <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel
+          className="form-select"
+          ref={inputLabel}
+          id="demo-simple-select-outlined-label"
+        >
+          {label}
+        </InputLabel>
+        <Select
+          className="form-list"
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={valueState}
+          onChange={handleChange}
+          labelWidth={labelWidth}
+        >
+          {items.map(item => (
+            <MenuItem className="select-item" value={item.value}>{item.value}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+FormSelect.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.string.isRequired,
+  })).isRequired,
+  valueState: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+FormSelect.defaultProps = {
+  label: '',
+  onChange: () => {},
+};
+
+export default FormSelect;
