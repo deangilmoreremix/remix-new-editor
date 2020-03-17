02402b192f6283dc13bd78df0d1ce211970aba3c
Sergey <persalius@gmail.com>
2020-03-17 14:06:17 +0200

    #1163890583803264 create FieldBuilder component
    edit component

--- components/form/FieldBuilder.jsx
+++ components/form/FieldBuilder.jsx
@@ -2,41 +2,42 @@ import React from 'react';
 import { useField } from 'formik';
 import PropTypes from '../../lib/PropTypes';
 // Inputs
-import FormCheckboxField from './FormCheckboxField';
 import FormColor from './FormColor';
-import FormList from './FormList';
+import FormRadioButton from './FormRadioButton';
 import FormTextField from './FormTextField';
+import FormSelect from './FormSelect';
 
 const inputs = {
   text: FormTextField,
-  select: FormList,
   color: FormColor,
-  checkbox: FormCheckboxField,
+  radio: FormRadioButton,
+  select: FormSelect
 };
 
 // ===== Component =====
 const FieldBuilder = (props) => {
-  const { type, placeholder, onChange } = props;
+  const { type, onChange } = props;
   const [field, meta, helpers] = useField(props);
 
   const handleChange = (value) => {
     helpers.setValue(value);
     onChange(value);
   };
+
   return (
     <div>
       {
-          type ?
-              inputs[type]({...field, ...props, onChange: handleChange})
-          : inputs.text({...field, ...props, onChange: handleChange})
+          type
+            ? inputs[type]({ ...field, ...props, onChange: handleChange })
+            : inputs.text({ ...field, ...props, onChange: handleChange })
       }
 
-      {/*<input*/}
-      {/*  type={type}*/}
-      {/*  {...field}*/}
-      {/*  placeholder={placeholder || field.name}*/}
-      {/*  onChange={handleChange}*/}
-      {/*/>*/}
+      {/* <input */}
+      {/*  type={type} */}
+      {/*  {...field} */}
+      {/*  placeholder={placeholder || field.name} */}
+      {/*  onChange={handleChange} */}
+      {/* /> */}
 
       {meta.touched && meta.error ? (
         <div className="field-error">{meta.error}</div>
