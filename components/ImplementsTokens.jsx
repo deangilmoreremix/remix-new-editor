f005bbcebe4e244ac3d7101529217df10ab1f9ea
Sergey <persalius@gmail.com>
2020-03-19 11:26:20 +0200

    #1163890583803257 Implement tokens edit component

--- components/ImplementsTokens.jsx
+++ components/ImplementsTokens.jsx
@@ -1,89 +1,82 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef, useEffect } from 'react';
 import classnames from 'classnames';
+import { systemDefaults, radioBtns } from '../lib/constants/implements_tokens';
 
-const systemDefaults = [
-    'firstname',
-    'lastname',
-    'email',
-    'geocountry',
-    'geocity',
-    'geostate',
-    'name',
-    'gender',
-    'custom'
-];
-
-const radioBtns = ['Plain', 'Fallback value:', 'UPPERCASE'];
 
 const ImplementsToken = () => {
-    const [listItem, setListItem] = useState(systemDefaults[0]);
-    const [radio, setRadio] = useState(radioBtns[0]);
-    const [activeInput, setActiveInput] = useState(true);
-    const inputRef = useRef(null);
+  const [listItem, setListItem] = useState(systemDefaults[0]);
+  const [radio, setRadio] = useState(radioBtns[0]);
+  const [activeInput, setActiveInput] = useState(true);
+  const inputRef = useRef(null);
 
-    const handleChangeRadio = value => async () => {
-        setRadio(value);
-        if (value === radioBtns[1]) {
-            await setActiveInput(false);
-            await inputRef.current.focus();
-        } else {
-            setActiveInput(true);
-        }
-    };
+  const handleChangeRadio = value => () => setRadio(value);
 
-    const handleChangeListItem = value => () => setListItem(value);
+  useEffect(() => {
+    if (radio === radioBtns[1]) {
+      (async () => {
+        await setActiveInput(false);
+        await inputRef.current.focus();
+      })();
+    } else {
+      setActiveInput(true);
+    }
+  }, [radio]);
 
-    const onInputFocus = () => handleChangeRadio(radioBtns[1]);
+  const handleChangeListItem = value => () => setListItem(value);
 
-    return (
-        <div className="implements">
-            <div className="implements__wrapper">
-                <div className="implements__header">
-                    <p>Personalizer</p>
-                    <button className='implements__close'>X</button>
-                </div>
-
-                <div className="implements__body">
-                    <ul className="implements__list">
-                        {
-                            systemDefaults.map(item => (
-                                <li
-                                    className={classnames('implements__item', {'implements__item-active': listItem === item})}
-                                    key={item}
-                                    onClick={handleChangeListItem(item)}
-                                >
-                                    {item}
-                                </li>
-                            ))
-                        }
-                    </ul>
-
-                    <div className="implements__info">
-                        <div>
-                            <p className="implements__name">{listItem || ''}</p>
-                            <ul>
-                                {
-                                    radioBtns.map(item => (
-                                        <li
-                                            className={classnames('implements__item', {'implements__item-active': radio === item})}
-                                            key={item}
-                                            onClick={handleChangeRadio(item)}
-                                        >
-                                            {item}
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                            <div className='implements__block-input' onClick={onInputFocus}>
-                                <input className="implements__input" type="text" disabled={activeInput} ref={inputRef} />
-                            </div>
-                        </div>
-                        <button className='implements__add'>add</button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="implements">
+      <div className="implements__wrapper">
+        <div className="implements__header">
+          <p>Personalizer</p>
+          <button className="implements__close" type="button">X</button>
         </div>
-    );
+
+        <div className="implements__body">
+          <div className="implements__list">
+            {
+              systemDefaults.map((item, i) => (
+                <button
+                  type="button"
+                  className={classnames('implements__item', { 'implements__item-active': listItem === item })}
+                  key={item}
+                  tabIndex={i}
+                  onClick={handleChangeListItem(item)}
+                >
+                  {item}
+                </button>
+              ))
+            }
+          </div>
+
+          <div className="implements__info">
+            <div>
+              <p className="implements__name">{listItem || ''}</p>
+              <div>
+                {
+                  radioBtns.map((item, i) => (
+                    <button
+                      type="button"
+                      className={classnames('implements__item', { 'implements__item-active': radio === item })}
+                      key={item}
+                      tabIndex={i}
+                      onClick={handleChangeRadio(item)}
+                    >
+                      {item}
+                    </button>
+                  ))
+                }
+              </div>
+              <div>
+                <input className="implements__input" type="text" disabled={activeInput} ref={inputRef} />
+              </div>
+            </div>
+            <button className="implements__add" type="button">add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImplementsToken;
