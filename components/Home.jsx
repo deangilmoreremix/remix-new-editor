6a919f86a55653177683557f2eccc7af759b58cd
Sergey <persalius@gmail.com>
2020-03-20 15:21:59 +0200

    #1163890583803257 Implement tokens

--- components/Home.jsx
+++ components/Home.jsx
@@ -13,7 +13,6 @@ import useModalStore from './hooks/useModalStore';
 
 import toolbarItems from '../lib/generators/toolbarItemsGenerator';
 import PlayButton from './common/timeline/PlayButton';
-import Personalization from "./Personalization";
 
 const getOne = async (store, id) => {
   await store.getOne(id);
@@ -62,7 +61,6 @@ const Home = observer(() => {
         <PlayButton />
       </Row>
       <Row className="timeline" noGutters />
-      <Personalization />
     </Container>
   );
 });
