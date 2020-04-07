c1778a670588f3d1ee6bbec8b6dcaa64e8d4518d
Katerina <ekaterina.maksimlyuk@vedidev.com>
2020-04-07 13:31:59 +0700

    #1154072706347831 - Easily save projects
    fixed menu

--- components/common/Menu.jsx
+++ components/common/Menu.jsx
@@ -3,15 +3,14 @@ import { observer } from 'mobx-react';
 import SVGInline from 'react-svg-inline';
 import { Popper, Button, Grow, MenuItem, MenuList, ClickAwayListener, Paper } from '@material-ui/core';
 
-import { useEffect } from 'react';
 import togglerIcon from '../../public/static/svgImages/common/toggler.svg';
 
 import PropTypes from '../../lib/PropTypes';
 
 const Menu = observer(({ toggleElement, items, className, needEndIcon, parent, placement }) => {
-  const [open, setOpen] = React.useState(false);
-  const [extraProps, setExtraProps] = React.useState({});
   const anchorRef = React.useRef(null);
+  const [open, setOpen] = React.useState(false);
+
   return (
     <div className={className || ''}>
       <Button
@@ -30,7 +29,15 @@ const Menu = observer(({ toggleElement, items, className, needEndIcon, parent, p
       >
         {toggleElement}
       </Button>
-      <Popper open={open} role={undefined} transition disablePortal className="popover" placement={placement || 'bottom'} anchorEl={parent ? parent.current : anchorRef.current}>
+      <Popper
+        open={open}
+        role={undefined}
+        transition
+        disablePortal
+        className="popover"
+        placement={placement || 'bottom'}
+        anchorEl={parent ? parent.current : anchorRef.current}
+      >
         {({ TransitionProps, placement: currentPlacement }) => (
           <Grow
             {...TransitionProps}
@@ -40,7 +47,10 @@ const Menu = observer(({ toggleElement, items, className, needEndIcon, parent, p
               <ClickAwayListener onClickAway={() => { setOpen(false); }}>
                 <MenuList id="menu-list-grow" onKeyDown={() => { setOpen(false); }}>
                   {items.map((item) => (
-                    <MenuItem key={`menu-${item.name}`} onClick={() => { item.onClick(); setOpen(false); }}>
+                    <MenuItem
+                      key={`menu-${item.title}`}
+                      onClick={() => { setOpen(false); }}
+                    >
                       {item.icon ? (
                         <SVGInline
                           className="icon"
@@ -69,7 +79,6 @@ Menu.propTypes = {
   items: PropTypes.arrayOrObservableArrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
     icon: PropTypes.string,
-    onClick: PropTypes.func.isRequired,
   })),
 };
 
