26855c79c6327c9cf0ef31de952484108942ca5b
Sergey <persalius@gmail.com>
2020-04-10 08:46:51 +0300

    #1167266412230865 Setting Layout

--- components/common/produce/SettingsPanel.jsx
+++ components/common/produce/SettingsPanel.jsx
@@ -1,27 +1,34 @@
 import * as React from 'react';
 import SVGInline from 'react-svg-inline';
+import { observer } from 'mobx-react';
 
 import PropTypes from '../../../lib/PropTypes';
+import useProjectStore from '../../hooks/useProjectStore';
 
-const SettingsPanel = ({ items }) => (
-  <div className="produce-block settings-panel">
-    {items.map(({ label, action, icon }) => (
-      <button
-        type="button"
-        key={label}
-        onClick={action}
-        className="settings-panel__button"
-      >
-        <SVGInline
-          className="settings-panel__icon"
-          svg={icon}
-          cleanup={['title']}
-        />
-        {label}
-      </button>
-    ))}
-  </div>
-);
+const SettingsPanel = observer(({ items }) => {
+  const { modified } = useProjectStore();
+
+  return (
+    <div className="produce-block settings-panel">
+      {items.map(({ label, action, icon }) => (
+        <button
+          type="button"
+          key={label}
+          onClick={action}
+          className="settings-panel__button"
+          disabled={modified && label.toLowerCase() !== 'list builder'}
+        >
+          <SVGInline
+            className="settings-panel__icon"
+            svg={icon}
+            cleanup={['title']}
+          />
+          {label}
+        </button>
+      ))}
+    </div>
+  );
+});
 
 SettingsPanel.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape({
