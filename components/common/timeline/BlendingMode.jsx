23f0f4b71df5ec95129e1ceb1e56be901d5adf36
Sergey <persalius@gmail.com>
2020-04-22 13:57:06 +0300

    #1171769496358865 Video Blending Modes

--- components/common/timeline/BlendingMode.jsx
+++ components/common/timeline/BlendingMode.jsx
@@ -3,16 +3,16 @@ import React, { useState } from 'react';
 import useProjectStore from '../../hooks/useProjectStore';
 
 import PropTypes from '../../../lib/PropTypes';
-import blendMode from '../../../lib/constants/blendMode';
+import fixBlendModes from '../../../lib/constants/blendMode';
 
 import Menu from '../Menu';
 
 const BlendingMode = ({ id }) => {
-  const [value, setValue] = useState(blendMode[0]);
+  const [value, setValue] = useState(fixBlendModes[0]);
   const { setBlendMode } = useProjectStore();
 
   const onChange = (mode) => {
-    blendMode.forEach(item => {
+    fixBlendModes.forEach(item => {
       if (item.value === mode) {
         setValue(item);
       }
@@ -23,7 +23,7 @@ const BlendingMode = ({ id }) => {
   return (
     <Menu
       toggleElement={value.title}
-      items={blendMode}
+      items={fixBlendModes}
       simpleBtn
       className="blend-mode-select"
       onClick={onChange}
