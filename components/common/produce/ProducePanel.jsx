3f3200d46408b9b30cb7fbfb77691013dbfd4ae0
Sergey <persalius@gmail.com>
2020-04-22 11:34:44 +0300

    #1167266412230865 Setting Layout

--- components/common/produce/ProducePanel.jsx
+++ components/common/produce/ProducePanel.jsx
@@ -7,25 +7,51 @@ import useProjectStore from '../../hooks/useProjectStore';
 
 const ProducePanel = observer(({ items }) => {
   const { modified } = useProjectStore();
+  console.log(modified);
 
   return (
     <div className="produce-block produce-panel">
-      {items.map(({ label, action, icon, alwaysOnDisplay }) => (
-        <button
-          type="button"
-          key={label}
-          onClick={action}
-          className="produce-panel__button"
-          disabled={modified && !alwaysOnDisplay}
-        >
-          <SVGInline
-            className="produce-panel__icon"
-            svg={icon}
-            cleanup={['title']}
-          />
-          {label}
-        </button>
-      ))}
+      {items.map((
+        { label, action, icon, alwaysOnDisplay },
+      ) => {
+        if (modified && alwaysOnDisplay) {
+          return (
+            <button
+              type="button"
+              key={label}
+              onClick={action}
+              className="produce-panel__button"
+            >
+              <SVGInline
+                className="produce-panel__icon"
+                svg={icon}
+                cleanup={['title']}
+              />
+              {label}
+            </button>
+          );
+        }
+
+        if (!modified) {
+          return (
+            <button
+              type="button"
+              key={label}
+              onClick={action}
+              className="produce-panel__button"
+            >
+              <SVGInline
+                className="produce-panel__icon"
+                svg={icon}
+                cleanup={['title']}
+              />
+              {label}
+            </button>
+          );
+        }
+
+        return null;
+      })}
     </div>
   );
 });
