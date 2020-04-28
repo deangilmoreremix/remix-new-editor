5ad015dcc29c9e22d2753adebca4c6feabf7f844
Sergey <persalius@gmail.com>
2020-04-28 11:06:45 +0300

    #1171769496358857 Canvas Guidelines

--- components/common/Guidelines.jsx
+++ components/common/Guidelines.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Guidelines = () => (
-  <div className="guidelines">
-    <div className="guidelines__square" />
+  <Fragment>
     <div className="guidelines__line guidelines__line--h" />
     <div className="guidelines__line guidelines__line--v" />
-  </div>
+    <div className="guidelines__side guidelines__side--l" />
+    <div className="guidelines__side guidelines__side--r" />
+    <div className="guidelines__side guidelines__side--t" />
+    <div className="guidelines__side guidelines__side--b" />
+  </Fragment>
 );
 
 export default Guidelines;
