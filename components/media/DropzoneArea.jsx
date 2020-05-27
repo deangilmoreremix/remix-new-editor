aafd7b3521b373baee3071157ebc26ae14e76ebe
aleksey leichenko <alekseyleychenko@gmail.com>
2020-05-27 12:46:31 +0300

    fixed bugs lB

--- components/media/DropzoneArea.jsx
+++ components/media/DropzoneArea.jsx
@@ -87,7 +87,7 @@ const DropzoneArea = (
             : (<p className="drag-drop__text">Drag and drop an image here, or click to upload</p>)
         }
         {
-          !image && !value && isArrows && (
+          (!image || !value) && isArrows && (
             <Fragment>
               <SVGInline className="drag-arrow drag-arrow-upper-left" svg={arrowIcon} cleanup={['arrow']} />
               <SVGInline className="drag-arrow drag-arrow-upper-right" svg={arrowIcon} cleanup={['arrow']} />
