eeababca22dc23136d9319be178003c7f3cf7c0c
Sergey Shevchenko <34631637+persalius@users.noreply.github.com>
2020-06-02 11:53:44 +0300

    #1177242044831859 Add audio/video settings (#180)
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings
    
    * #1177242044831859 Add audio/video settings

--- components/media/Library.jsx
+++ components/media/Library.jsx
@@ -358,6 +358,7 @@ const Library = observer(() => {
           userContentTitle={tabItems[activeTab].label}
           handleButtonClick={handleButtonClick}
           list={listProviders}
+          activeTab={activeTab}
         />
       );
     }
