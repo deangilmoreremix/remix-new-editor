082bd0ceb0889c14b3348e9c7acfbd827cc070d5
Sergey <persalius@gmail.com>
2020-06-18 07:23:06 +0300

    #1180579111268901 add presets library in SVR

--- components/modals/Presets/PresetsList.jsx
+++ components/modals/Presets/PresetsList.jsx
@@ -3,29 +3,27 @@ import { Waypoint } from 'react-waypoint';
 
 import PropTypes from '../../../lib/PropTypes';
 
-const PresetsList = ({ items, hasMore, uploadNewItems, handleSelect }) => {
-  return (
-    <div className="presets-list">
-      {items && items.length
-        ? (
-          <Fragment>
-            {items.map((item) => (
-              <button
-                key={item._id}
-                className="presets-list__button"
-                onClick={() => handleSelect(item)}
-              >
-                {item.project.name}
-              </button>
-            ))}
-          </Fragment>
-        )
-        : null}
+const PresetsList = ({ items, hasMore, uploadNewItems, handleSelect }) => (
+  <div className="presets-list">
+    {items && items.length
+      ? (
+        <Fragment>
+          {items.map((item) => (
+            <button
+              key={item._id}
+              className="presets-list__button"
+              onClick={() => handleSelect(item)}
+            >
+              {item.project.name}
+            </button>
+          ))}
+        </Fragment>
+      )
+      : null}
 
-      {hasMore && <Waypoint bottomOffset="3%" onEnter={uploadNewItems}><span className="preset-waypoint" /></Waypoint>}
-    </div>
-  );
-};
+    {hasMore && <Waypoint bottomOffset="3%" onEnter={uploadNewItems}><span className="preset-waypoint" /></Waypoint>}
+  </div>
+);
 
 PresetsList.propTypes = {
   items: PropTypes.arrayOrObservableArray,
