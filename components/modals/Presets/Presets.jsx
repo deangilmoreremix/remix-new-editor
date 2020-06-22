a982275abed1307b0cc14234e199aea1ac80575a
Sergey <persalius@gmail.com>
2020-06-22 09:59:39 +0300

    #1180579111268901 add presets library in SVR

--- components/modals/Presets/Presets.jsx
+++ components/modals/Presets/Presets.jsx
@@ -2,8 +2,6 @@ import React, { useCallback, useState } from 'react';
 
 import { perPage } from '../../../lib/constants/library';
 
-import PropTypes from '../../../lib/PropTypes';
-
 import useMakeStore from '../../hooks/useMakeStore';
 import usePresetStore from '../../hooks/usePresetStore';
 import useProjectStore from '../../hooks/useProjectStore';
@@ -12,6 +10,8 @@ import { showError } from '../../../lib/services/alertService';
 import PresetsList from './PresetsList';
 import PresetsPreview from './PresetsPreview';
 
+
+// eslint-disable-next-line react/prop-types
 const Presets = ({ handleClose }) => {
   const [items, setItems] = useState([]);
   const [activeItem, setActiveItem] = useState();
@@ -116,10 +116,6 @@ const Presets = ({ handleClose }) => {
       </div>
     </div>
   );
-};
-
-Presets.propTypes = {
-  handleClose: PropTypes.func,
 };
 
 export default Presets;
