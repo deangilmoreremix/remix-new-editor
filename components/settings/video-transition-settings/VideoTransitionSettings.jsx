f70b18cd36fdf55665a178ee1d9b053b59595491
Sergey <persalius@gmail.com>
2020-07-02 10:53:07 +0300

    #1179306064519849 First added video is getting shrink when applying transition

--- components/settings/video-transition-settings/VideoTransitionSettings.jsx
+++ components/settings/video-transition-settings/VideoTransitionSettings.jsx
@@ -110,7 +110,7 @@ const VideoTransitionSettings = observer(({ element, update, fields, find }) => 
 
   React.useEffect(() => {
     if (fromPlayer && fromPlayer.current && !isCaptured) {
-      fromPlayer.current.video.seek(fromVideo.popcornOptions.end);
+      fromPlayer.current.video.seek(fromVideo.popcornOptions.end - fromVideo.popcornOptions.start);
     }
   }, [fromVideo, isCaptured]);
 
