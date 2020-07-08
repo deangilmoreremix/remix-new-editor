10fad0e3a38ff1aabdcb485ca4f7adec89350818
Sergey <persalius@gmail.com>
2020-07-08 20:16:49 +0300

    #1182975146477578 New editor. Add new popcorn element

---
+++ components/common/timeline/elements/OverlayElement.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import classnames from 'classnames';
+import Grid from '@material-ui/core/Grid/Grid';
+import SVGInline from 'react-svg-inline';
+
+import PropTypes from '../../../../lib/PropTypes';
+
+import svgOverlayIcon from '../../../../public/static/images/toolbar/overlay.svg';
+import { POPCORN_ELEMENT_LABELS } from '../../../../lib/constants/popcorn';
+
+const IconElement = React.forwardRef(({ item, onSelect, ...rest }, ref) => (
+  <Grid
+    container
+    className="popcorn-element icon-element"
+    onClick={onSelect}
+    ref={ref}
+    title={item.type}
+    tabIndex={-1}
+    {...rest}
+  >
+    <div className={classnames('inner-wrapper', 'popcorn-pause')}>
+      <SVGInline
+        className="icon-btn"
+        classSuffix="--inline"
+        svg={svgOverlayIcon}
+        cleanup={['title']}
+      />
+    </div>
+    <div>
+      {POPCORN_ELEMENT_LABELS[item.type]}
+    </div>
+  </Grid>
+));
+
+IconElement.propTypes = {
+  item: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    htmlText: PropTypes.string,
+    duration: PropTypes.number,
+  }).isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+export default IconElement;
