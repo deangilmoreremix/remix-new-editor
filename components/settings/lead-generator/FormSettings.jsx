f64c26e19ddb710fb04841e3b7a4226eb70c1e49
Alexei Leichenko <alekseyleychenko@gmail.com>
2020-09-24 16:02:44 +0300

    fixed policy (#454)

--- components/settings/lead-generator/FormSettings.jsx
+++ components/settings/lead-generator/FormSettings.jsx
@@ -36,6 +36,7 @@ const FormSettings = observer(({ tab = STYLES, element, update, fields, handleCl
           onChange={(field, options) => handleChange(field, options)}
           fields={fields}
           closeModal={handleClose}
+          kind={element?.kind || ''}
         />
       )}
     </div>
@@ -45,6 +46,8 @@ const FormSettings = observer(({ tab = STYLES, element, update, fields, handleCl
 FormSettings.propTypes = {
   element: PropTypes.shape({
     options: PropTypes.shape({}).isRequired,
+    popcornOptions: PropTypes.shape({}),
+    kind: PropTypes.string,
   }).isRequired,
   fields: PropTypes.shape({}).isRequired,
   tab: PropTypes.string,
