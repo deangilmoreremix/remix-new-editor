2ea7ece1fb7fa6a4458955b081931c2d512e98e2
Sergey Shevchenko <34631637+persalius@users.noreply.github.com>
2020-11-10 09:24:13 +0200

    Fix connect (#622)

--- globals/stores/project.store.js
+++ globals/stores/project.store.js
@@ -1064,6 +1064,7 @@ export default class ProjectStore extends BaseStore {
             zindex: null,
             start: null,
             end: null,
+            type: trackEvent.type,
           };
 
           if (trackEvent.popcornOptions.start === firstElementStart) {
