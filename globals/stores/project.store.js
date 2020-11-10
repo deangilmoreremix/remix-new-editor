5a30699b4583a65dfaca1e948c2bcc185b9e9642
Alexey <36531596+TheRadly@users.noreply.github.com>
2020-11-10 11:36:50 +0300

    #1199085106819864 - Disabled disclaimer text in personalizer (#617)

--- globals/stores/project.store.js
+++ globals/stores/project.store.js
@@ -356,7 +356,7 @@ export default class ProjectStore extends BaseStore {
   };
 
   @action
-  createRetargetForm = (noUndo, kind) => {
+  createRetargetForm = (noUndo, kind, isPersonalizer) => {
     if (!noUndo) {
       this.setUndo();
     }
@@ -382,6 +382,7 @@ export default class ProjectStore extends BaseStore {
     // eslint-disable-next-line no-underscore-dangle
     popcornFunctions._setup(retargetOptions);
     this.retarget = { ...retargetOptions, ...popcornFunctions };
+    this.retarget.isPersonalizer = isPersonalizer;
     this.retarget.end = () => {
       this.showedRetarget = false;
       if (popcornFunctions.end) {
@@ -413,7 +414,11 @@ export default class ProjectStore extends BaseStore {
 
 
   @action
-  addRetargetForm = ({ kind, showed, noUndo }) => {
+  addRetargetForm = ({ kind, showed, noUndo, isPersonalizer }) => {
+    if (!this.retarget || (this.retarget && !this.retarget.id) || !showed) {
+      console.log(isPersonalizer);
+      this.createRetargetForm(noUndo, kind, isPersonalizer);
+    }
     if (this.retarget && this.retarget.options && this.retarget.id) {
       const isAdvancedOptin = kind === POPCORN_ELEMENT_TYPES.ADVANCED_OPTIN;
       Object.keys(DEFAULT_OPTIONS_OPTIN).forEach(key => {
@@ -428,12 +433,10 @@ export default class ProjectStore extends BaseStore {
           }
         }
       });
+      this.retarget.isPersonalizer = isPersonalizer;
       // eslint-disable-next-line no-underscore-dangle
       this.retarget._update({ ...this.retarget }, { ...this.retarget.options });
       this.releaseElement();
-    }
-    if (!this.retarget || (this.retarget && !this.retarget.id) || !showed) {
-      this.createRetargetForm(noUndo, kind);
     }
     this.retarget.kind = kind;
     this.editElement(this.retarget.id);
