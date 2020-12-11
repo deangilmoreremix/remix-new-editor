18f28fc89dd02e1b4bde1917c3ea5387ee6a54cc
Katerina <ekaterina.maksimlyuk@vedidev.com>
2020-12-11 13:16:54 +0700

    Stable snapshot

--- globals/stores/project.store.js
+++ globals/stores/project.store.js
@@ -746,8 +746,8 @@ export default class ProjectStore extends BaseStore {
     const elements = [];
     const projectData = data;
     projectData.media.forEach((media) => {
-      media.tracks = _.uniqWith(media.tracks, _.isEqual);
       media.tracks.forEach((track) => {
+        track.trackEvents = _.uniqWith(track.trackEvents, _.isEqual);
         track.trackEvents.forEach((trackEvent) => {
           elements.push({
             ...trackEvent,
