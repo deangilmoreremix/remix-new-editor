8b5b361790925a540ff73da1b0745ea307ab0bf8
Alexei Leichenko <alekseyleychenko@gmail.com>
2020-12-24 09:07:37 +0200

    #1198898272731882 - More than three or four personalized elements in text masking display blank screen (#747)
    * max length
    
    * max length
    
    * changed to snack bar
    
    * added prop-types
    
    * created separated component withWarning
    
    * fixed
    
    * changed name
    
    * add component
    
    * redid logic for warning/error component
    
    * redid logic for warning/error component
    
    * redid logic for warning/error component
    
    * redid logic for warning/error component
    
    * redid logic for warning/error component
    
    * changed name

--- components/hoc/withValidation.jsx
+++ components/hoc/withValidation.jsx
@@ -1,26 +1,44 @@
 import * as React from 'react';
 
 import Error from '../common/snackBars/Error';
+import Warning from '../common/snackBars/Warning';
 
 import * as VALIDATORS from '../../lib/validators';
 
+import { VALIDATION_TYPES } from '../../lib/constants/validator';
+
 const required = VALIDATORS.required();
+
+const messageType = {
+  [VALIDATION_TYPES.WARNING]: Warning,
+  [VALIDATION_TYPES.ERROR]: Error,
+};
 
 const withValidation = (WrappedComponent) => (props) => {
   const [error, setError] = React.useState(null);
+  const [errorType, setErrorType] = React.useState(VALIDATION_TYPES.ERROR);
+
+  const Snack = React.useMemo(() => messageType[errorType], [errorType]);
 
   const checkValue = (value, options = {}) => {
-    const { type = null, isRequired = false } = options;
+    const {
+      type = null,
+      isRequired = false,
+      message,
+      validationType = VALIDATION_TYPES.ERROR,
+    } = options;
     if (isRequired) {
       const e = required(value);
       if (e) {
+        setErrorType(validationType);
         setError(e);
         return e;
       }
     }
     if (type) {
-      const e = VALIDATORS.default[type]({ value });
+      const e = VALIDATORS.default[type]({ value, message });
       if (e) {
+        setErrorType(validationType);
         setError(e);
         return e;
       }
@@ -36,7 +54,7 @@ const withValidation = (WrappedComponent) => (props) => {
           setError(err);
         }}
       />
-      <Error {...props} message={error} handleClose={() => setError(null)} />
+      <Snack {...props} message={error} handleClose={() => setError(null)} />
     </React.Fragment>
   );
 };
