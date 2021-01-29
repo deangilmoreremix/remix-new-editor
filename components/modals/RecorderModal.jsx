781794c9433045a3c3a1e0c228db4484ca9a85dd
Alexey <36531596+TheRadly@users.noreply.github.com>
2021-01-29 12:47:01 +0300

    #1182414573725889 - Fixed incorrect display PiP button in Recorder modal (#861)

--- components/modals/RecorderModal.jsx
+++ components/modals/RecorderModal.jsx
@@ -57,6 +57,7 @@ export default observer(({ options: { type, useAudio }, handleClose }) => {
   const [showHiddenButton, setShowHiddenButton] = useState(false);
   const [time, setTime] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [showPipButton, setShowPipButton] = useState(false);
 
   let { current: player } = playerRef;
 
@@ -79,6 +80,9 @@ export default observer(({ options: { type, useAudio }, handleClose }) => {
 
       player.on('deviceReady', () => {
         setShowHiddenButton(true);
+        if (config.plugins.record.video !== false) {
+          setShowPipButton(true);
+        }
       });
 
       player.recordToggle.on('click', () => {
@@ -251,13 +255,13 @@ export default observer(({ options: { type, useAudio }, handleClose }) => {
                   playsInline
                 />
                 {showHiddenButton && (
-                  <>
-                    <button className="recorder-button-hidden" onClick={handleClick} />
-                    <button
-                      onClick={() => videoRef.current.requestPictureInPicture()}
-                      className="pic-to-pic vjs-pip-button vjs-control vjs-button vjs-icon-picture-in-picture-start"
-                    />
-                  </>
+                  <button className="recorder-button-hidden" onClick={handleClick} />
+                )}
+                {showPipButton && (
+                  <button
+                    onClick={() => videoRef.current.requestPictureInPicture()}
+                    className="pic-to-pic vjs-pip-button vjs-control vjs-button vjs-icon-picture-in-picture-start"
+                  />
                 )}
               </div>
               <div className={`recorder-modal-options ${saveOptionsVisible ? '' : 'recorder-modal-options_hidden'}`}>
