1f611741717983939b7ce190f433ff02ca8e3630
aleksey-lav <alekseyleychenko@gmail.com>
2021-02-11 09:55:49 +0200

    #1199702939780949 - Add Facebook pixels ability /  analytic to the editor

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,18 +2,53 @@ import React from 'react';
 
 import MenuAppBar from './MenuAppBar';
 
+import { DOMAIN_VIDEOREMIX } from '../lib/constants/project';
+import useUserStore from './hooks/useUserStore';
+
 import PropTypes from '../lib/PropTypes';
 
-function Header({ whiteLabelManager }) {
+function Header({ whiteLabelManager, fbPixelId, scriptStatistic }) {
+  const { currentUser } = useUserStore();
+
   return (
     <header className="menu-app-bar">
       <MenuAppBar whiteLabelManager={whiteLabelManager} />
+      {whiteLabelManager.domain === DOMAIN_VIDEOREMIX
+      && scriptStatistic && (
+        <>
+          <script dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', ${fbPixelId});
+                  fbq('track', 'PageView', {'current_user_id': '${currentUser.id}', current_user_email: '${currentUser.email}'});`,
+          }}
+          />
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<img
+                    height="1"
+                    width="1"
+                    style="display:none"
+                    src='https://www.facebook.com/tr?id=${fbPixelId}&ev=PageView&noscript=1'/>`,
+          }}
+          />
+        </>
+      )}
     </header>
   );
 }
 
 Header.propTypes = {
-  whiteLabelManager: PropTypes.shape({}),
+  whiteLabelManager: PropTypes.shape({
+    domain: PropTypes.string,
+  }),
+  fbPixelId: PropTypes.number,
+  scriptStatistic: PropTypes.bool,
 };
 
 export default Header;
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -80,16 +80,6 @@ class Layout extends Component {
               />`,
                 }}
                 />
-                {this.stores.common.facebookPixelId && (
-                  <noscript dangerouslySetInnerHTML={{
-                    __html: `<img
-                    height="1"
-                    width="1"
-                    style="display:none"
-                    src='https://www.facebook.com/tr?id=${this.stores.common.facebookPixelId}&ev=PageView&noscript=1'/>`,
-                  }}
-                  />
-                )}
               </>
             )}
             <div className={`theme-${whiteLabelManager.key} ${layoutClassName} ${className}-baseheader`}>
@@ -165,21 +155,6 @@ class Layout extends Component {
                     fbq('track', 'PageView');`,
                     }}
                     />
-                    {this.stores.common.facebookPixelId && (
-                      <script dangerouslySetInnerHTML={{
-                        __html: `!function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', ${this.stores.common.facebookPixelId});
-                  fbq('track', 'PageView', {'current_user_id': '${this.currentUser.id}', current_user_email: '${this.currentUser.email}'});`,
-                      }}
-                      />
-                    )}
                   </>
                 )}
                 {/* End Facebook Pixel Code */}
@@ -192,6 +167,8 @@ class Layout extends Component {
                       <Header
                         whiteLabelManager={whiteLabelManager}
                         className={`theme-${whiteLabelManager.key}`}
+                        fbPixelId={+this.stores.common.facebookPixelId}
+                        scriptStatistic={this.stores.common.scriptStatistic === 'true'}
                       />
                     ) }
                   <div {...rest} className={`main theme-${whiteLabelManager.key}`}>
