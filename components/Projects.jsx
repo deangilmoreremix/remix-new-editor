1a19e08bacb1b32b296a1d1333d177171507af72
Alexey <36531596+TheRadly@users.noreply.github.com>
2021-03-23 08:59:04 +0300

    #1199686248436456 - Implement folders sidebar to projects page (#931)
    * #1199686248436456 - Added folders to projects
    
    * #1199686248436456 - Implement create folder modal to projects
    
    * #1199686248436456 - Added folder modal & added logic for delete, edit folder
    
    * #1199686248436456 - Added update & remove request to backend
    
    * #1199686248436456 - Fix errors & added white labels
    
    * #1199686248436456 - Single method for component prop
    
    * #1199686248436456 - Fix some errors
    
    * #1199686248436456 - Rename file name & fix some bugs
    
    * #1199686248436456 - Fix select item
    
    * #1199686248436485 - Update logic for added items!
    
    * #1199686248436485 - Fix some errors
    
    * #1199686248436456 - Remove folder name condition from func
    
    * #1199686248436456 - Added styles for folders & archived item
    
    * #1199686248436456 - Fix some styles for list
    
    * #1199686248436456 - Change height
    
    * #1199686248436485 - Added sorted items & fix styles for OK button and folders
    
    * #1199686248436485 - Fix gradient styles
    
    * #1199686248436485 - Fix folder name length & add item in start pos array
    
    * #1199686248436485 - Fix typo

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,18 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { observer } from 'mobx-react';
 import classnames from 'classnames';
 
 import { initialState as listInitialState, reducer as listReducer } from '../lib/utils/reducers/listReducer';
+
 import { ACTION_TYPES } from '../lib/constants/reducers/listReducer';
+import { FOLDER_MODAL } from '../lib/constants/modals';
+import { showSuccess, showConfirmation } from '../lib/services/alertService';
+
 import useUserStore from './hooks/useUserStore';
+import useModalStore from './hooks/useModalStore';
+import useBaseStore from './hooks/useBaseStore';
 
 import ProjectsPreview from './common/libraryElements/ProjectsPreview';
 import List from './common/gallery/List';
+import Folders from './common/Folders';
+import Folder from './common/libraryElements/Folder';
+
+const ARCHIVED = 'archived';
 
 const Projects = observer(() => {
   const { hasPermissions } = useUserStore();
+  const { openModal } = useModalStore();
+  const { sendRequest } = useBaseStore();
 
   const [list, dispatchList] = React.useReducer(listReducer, listInitialState);
+  const [foldersList, dispatchFoldersList] = React.useReducer(listReducer, listInitialState);
+
   useEffect(() => {
     dispatchList({
       type: ACTION_TYPES.SET_INITIAL,
@@ -20,8 +34,101 @@ const Projects = observer(() => {
     });
   }, []);
 
+  useEffect(() => {
+    updateFolders();
+  }, []);
+
+  useEffect(() => {
+    if (!foldersList.activeItem) {
+      selectFolder();
+    }
+  }, [foldersList.activeItem]);
+
+  const selectFolder = useCallback((item) => {
+    dispatchFoldersList({
+      type: ACTION_TYPES.SET_ACTIVE_ITEM,
+      value: item || null,
+    });
+
+    if (item === ARCHIVED) {
+      dispatchList({
+        type: ACTION_TYPES.UPDATE_FILTER,
+        value: { key: ARCHIVED, v: { $in: true } },
+      });
+    } else {
+      dispatchList({
+        type: ACTION_TYPES.UPDATE_FILTER,
+        value: { key: ARCHIVED, v: { $in: [null, false] } },
+      });
+      dispatchList({
+        type: ACTION_TYPES.UPDATE_FILTER,
+        value: { key: 'folder', v: item && item._id },
+      });
+    }
+  }, [list.filter]);
+
+  const onDeleteFolder = async (item) => {
+    const response = await showConfirmation(
+      'Are you sure you want to delete the folder? All projects here in will be deleted as well.',
+      `Delete "${item.title}" folder`,
+    );
+
+    if (response) {
+      await sendRequest(
+        'DELETE',
+        `/api/users/me/folders/${item._id}`,
+      );
+      updateFolders();
+      showSuccess(`Folder "${item.title}" is successfully deleted!`);
+    }
+  };
+
+  const updateFolders = () => {
+    dispatchFoldersList({
+      type: ACTION_TYPES.SET_INITIAL,
+      value: {
+        path: '/api/users/me/folders',
+        content: (props) => (
+          <Folder
+            onClick={selectFolder}
+            onDelete={onDeleteFolder}
+            onEdit={onEditFolder}
+            {...props}
+          />
+        ),
+        perPage: 25,
+        orderBy: { createdAt: -1 },
+      },
+    });
+  };
+
+  const updateFolder = (folder) => {
+    dispatchFoldersList({
+      type: ACTION_TYPES.UPDATE_ITEM,
+      value: folder,
+    });
+  };
+
+  const createFolder = (folder) => {
+    dispatchFoldersList({
+      type: ACTION_TYPES.ADD_ITEM,
+      value: folder,
+    });
+  };
+
+  const onEditFolder = (item) => {
+    openModal(FOLDER_MODAL, { item, updateFolder });
+  };
+
   return (
     <div className={classnames('projects', { 'dark-theme': hasPermissions })}>
+      <Folders
+        list={foldersList}
+        dispatchList={dispatchFoldersList}
+        select={selectFolder}
+        className="categories"
+        createFolder={createFolder}
+      />
       <div className="list">
         <List
           list={list}
@@ -30,7 +137,7 @@ const Projects = observer(() => {
           contentClassName="library__items"
         />
       </div>
-      <div className="library__gradient" />
+      <div className="library__gradient only-content" />
     </div>
   );
 });
