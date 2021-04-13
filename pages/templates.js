b00c79d748a5994e38ff0f3ac2564622a845435b
EkaterinaMaksimlyuk <34784502+EkaterinaMaksimlyuk@users.noreply.github.com>
2021-04-13 11:35:49 +0700

    Stable snapshot (#952)
    * #1200110632717249 - Order of Width and Height is incorrect on share window. (#948)
    
    * #1200150582153895 Create new editor for List Buster (#949)
    
    removed templates/projects from revolution
    
    * test with thumbnail editor (#950)
    
    * revert background image (#951)
    
    Co-authored-by: Alexei Leichenko <alekseyleychenko@gmail.com>

--- pages/templates.js
+++
@@ -1,5 +0,0 @@
-import pageFactory from '../components/hoc/pageFactory';
-import Templates from '../components/Templates';
-import ListHeader from '../components/ListHeader';
-
-export default pageFactory({ RootComponent: Templates, Header: ListHeader, className: 'templates' });
