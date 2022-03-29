144ea2096b99b6b370bfd19a625a5fc2bdd24342
olamiBounty <olamiposi.olorunsola@gmail.com>
2022-03-29 15:15:17 +0100

    fix loading, theme adn license issue

--- components/common/ImglyImageEditor.js
+++ components/common/ImglyImageEditor.js
@@ -9,6 +9,7 @@ import PropTypes from '../../lib/PropTypes';
 import { tabItems } from '../../lib/constants/library';
 
 
+const license = { api_token: "Bmz_MBXK-7Rqsro1eLceKg", app_identifiers: ["revolution.videoremix.io", "dev-revolution.videoremix.io", "local-smart-video.videoremix.io"], available_actions: [], domains: ["https://api.photoeditorsdk.com"], enterprise_license: false, expires_at: null, features: ["camera", "library", "export", "customassets", "whitelabel", "adjustment", "brush", "filter", "focus", "frame", "overlay", "sticker", "text", "textdesign", "transform"], issued_at: 1648560818, minimum_sdk_version: "1.0", owner: "VideoRemix", platform: "HTML5", products: ["pesdk"], version: "2.4", signature: "aTxspPl/8xoZ7PgshpJWadw14FyTNqPONPd9aLv1tM0FxPbnZ9SLZ1dy/kkdUfu5E+jIt1DOio6CsBhF+tzmnG/OO06T57DT+p4OiHZfZWIy0StukcbZp/neAg7/bF9joXayKMBAi1r8Vcd5dMiGLYwyTe4znGrSZPxvTmaRrixBxDFCXPNMJFyziTeFa9+WlEG8duw0TSqYsaQkqZmw0/QIhiusBTRuy3W/HqUn074UpGtJ1CT7qbUhr2bFvwuE4HgbhlNn939VzF/wsMA79sML8GimpQKBhMuZOIR+wDbG8yT80/JHCcKIKqP3xujJzG7RPCZ0pmwr4nrfYu+r7HZ8y5LBRllLhQJZsXs3zxUn+FzQj61u0r1yeuCQOjyYrFqCV+5qAiS85UdYzsFiYLV3rYv0HxffM+LFhmJzCY5tivLTKuwYoIBtLmD5bne+Pke8anBZM8TCvu+2hVA0HHvVPkQTkjLd7nbFmLwQ885tUdotogJ6D7/Eg5Yi/GJ+Pi32NMGOVNhWPYF470wGd2e/CnyPvylTh4IS6FM2rrI1PvmJnOKTRhsuO5luw+E9Gc8ZxKWAOYDALWzUQjILtQLOS8XFadPuFjlGu248bmiozExfzTt2hupb0PZrCHm4GvcGgPSLiobYeXuBqXEycmsSFUjR4JPtypw9Jv0Qq2o=" };
 const PhotoEditorSDK = observer(({
   imageData,
   onImageEdited,
@@ -61,9 +62,7 @@ const PhotoEditorSDK = observer(({
     const editor = await PhotoEditorSDKUI.init({
       container: '#editor',
       image: source, // Image url or Image path relative to assets folder
-      license: `{"api_token":"Bmz_MBXK-7Rqsro1eLceKg","app_identifiers":["https://videoremix.io/","https://revolution.videoremix.io/","https://revolution.videoremix.io/edit","https://lead-buster.videoremix.io/","https://dev-revolution.videoremix.io/","https://dev-revolution.videoremix.io/edit","http://local-smart-video.videoremix.io/","http://local-smart-video.videoremix.io/edit"],"available_actions":[],"domains":["https://api.photoeditorsdk.com"],"enterprise_license":false,"expires_at":null,"features":["camera","library","export","customassets","whitelabel","adjustment","brush","filter","focus","frame","overlay","sticker","text","textdesign","transform"],"issued_at":1648111355,"minimum_sdk_version":"1.0","owner":"VideoRemix","platform":"HTML5","products":["pesdk"],"version":"2.4","signature":"OvKhhfYiRHcDZ/GggObgMKd4r+p8Jh4JxP3Ee5OWKpFlMReksa7BCrBpbM8D+6wB8e9BYqNcjutNqyzjLHWAI8z2Z5fqDoTCkF/rnG/HBZ5eTU6DXS+hn6mOs2Aecx3LpOtrGh96vXEwsPExbqauODTxUE2IWneiZywek1e54Pvqw6cEmaGoGCY2cFNkkKx/HjgGdAdmP2BUNhlYXhUzZmsN0L64G4i32yO2caCWpSSLsvRXOML6cpf9qLCX8+ccuFPPi45n5KcrU/m50/fln8cIgBWNeiuFLgyiqZSe2hs4SluNAhvdhS9xZSxpvd4KciPxXfl83ILliAC8FwqpzgeSdpK59QcxE3QfJbuM37guK4s/DZGQtwV4tUjOHZdqWbYv4sIKvpkhcjcYwwt2JUzLmaH0GSjyz92Te6LZlbFAqoSr+4xxuEvtTwQ9iliVBhq4v/zIYFu8roCEAAFrRTeCPf7kq0mw7Yf7PrJhkEHEjN2SQn5QdvvTzcVDNFJspWYNWnf47nl6I6DnHtQf/Vz4lWFblI1oAQpVMubM66rhWms4LEKEQvELWNk58/8cc4xG1ywhYMke5xJ4TP9kw9bM1aD/TcWtqEkdaz0OHl/wgkm0n1njuzbGWEO/ClYBcQVgNl4zfBLz4ry+bZX/7TkBv63l9b+EnkzRA3+BlCk="}
-      `,
-      // mainCanvasActions: ['export', 'download'],
+      license: JSON.stringify(license),
       export: {
         image: {
           exportType: 'blob',
@@ -74,6 +73,15 @@ const PhotoEditorSDK = observer(({
         mainCanvasActions: ['undo', 'redo', 'export', 'close'],
         themes: {
           dark: {
+            /** Base Colors */
+            /** Overall background color. This color is used if not overriden by a component */
+            background: '#000000',
+            /** Overall foreground color. This color is used if not overriden by a component */
+            foreground: '#FFFFFF',
+            primary: '#EB4F53',
+            warning: '#FFBA5C',
+            error: '#C2393A',
+            success: '#39C26C',
             tooltip: {
               background: 'darkgray',
             },
