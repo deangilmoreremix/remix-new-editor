08cab8cb95d13a51c773360fbda3f86fd48b246c
olamiBounty <olamiposi.olorunsola@gmail.com>
2022-04-11 10:31:44 +0100

    fix roles and features image lt

--- globals/stores/make.store.js
+++ globals/stores/make.store.js
@@ -80,4 +80,8 @@ export default class PresetStore extends BaseStore {
   getConnect = ({ page = 1, query = '', perPage = 12 }) => (
     this.getList({ page, query, perPage, params: { segment: makeTypes.CONNECT }, path: '/api/makes/revolution' })
   );
+
+  getImageLTPreset = ({ page = 1, query = '', perPage = 12 }) => (
+    this.getList({ page, query, perPage, params: { segment: makeTypes.EVN_IMAGE_LT_PRESETS }, path: '/api/makes/revolution' })
+  );
 }
