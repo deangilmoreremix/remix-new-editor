4961082f54ab3814a93e1d31b9147512ee00c55c
Himanshu Rathod <himanshu@videoremix.io>
2022-09-21 18:50:49 +0530

    Added new exemplers

--- components/common/AdvanceImageEditor/PhotoAnimer.jsx
+++ components/common/AdvanceImageEditor/PhotoAnimer.jsx
@@ -306,54 +306,54 @@ const PhotoEnhancer = observer(({
       id: 13,
     },
 
-    // {
-    //   name: 'Animer14',
-    //   src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/14.mp4',
-    //   id: 14,
-    // },
+    {
+      name: 'Animer14',
+      src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/14.mp4',
+      id: 'gaoxiao1',
+    },
 
-    // {
-    //   name: 'Animer15',
-    //   src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/15.mp4',
-    //   id: 15,
-    // },
-
-
-    // {
-    //   name: 'Animer16',
-    //   src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/16.mp4',
-    //   id: 16,
-    // },
-
-    // {
-    //   name: 'Animer17',
-    //   src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/17.mp4',
-    //   id: 17,
-    // },
-
-    // {
-    //   name: 'Animer18',
-    //   src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/18.mp4',
-    //   id: 18,
-    // },
-    // {
-    //   name: 'Animer19',
-    //   src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/19.mp4',
-    //   id: 19,
-    // },
-
-    // {
-    //   name: 'Animer20',
-    //   src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/20.mp4',
-    //   id: 20,
-    // },
+    {
+      name: 'Animer15',
+      src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/15.mp4',
+      id: 'gaoxiao2',
+    },
 
 
-    // {
-    //   name: 'Animer21',
-    //   src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/21.mp4',
-    //   id: 21,
-    // },
+    {
+      name: 'Animer16',
+      src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/16.mp4',
+      id: 'gaoxiao3',
+    },
+
+    {
+      name: 'Animer17',
+      src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/17.mp4',
+      id: 'gaoxiao4',
+    },
+
+    {
+      name: 'Animer18',
+      src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/18.mp4',
+      id: 'gaoxiao5',
+    },
+    {
+      name: 'Animer19',
+      src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/19.mp4',
+      id: 'gaoxiao6',
+    },
+
+    {
+      name: 'Animer20',
+      src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/20.mp4',
+      id: 'gaoxiao7',
+    },
+
+
+    {
+      name: 'Animer21',
+      src: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/animer/animer/21.mp4',
+      id: 'gaoxiao8',
+    },
 
 
   ];
