eff2507d3eaca24f9f0f194227cce808bceb9f58
Himanshu Rathod <himanshu@videoremix.io>
2022-11-28 11:08:29 +0530

    Added tooltip for 360 video

--- components/settings/video-settings/components/Is360.jsx
+++ components/settings/video-settings/components/Is360.jsx
@@ -5,6 +5,9 @@ import classnames from 'classnames';
 import PropTypes from '../../../../lib/PropTypes';
 import FormCheckboxField from '../../../form/FormCheckboxField';
 import VRIcon from '../../../../public/static/images/media/360-degrees.svg';
+import { videoTooltips } from '../../../../lib/constants/tooltips';
+import HelpIconComponent from '../../../common/HelpIcon';
+
 
 const Is360 = React.memo(({
   value,
@@ -20,6 +23,8 @@ const Is360 = React.memo(({
         value={value}
         floatClassName={className}
       />
+      <HelpIconComponent noDelay noIcon message={videoTooltips.value
+      }>
       <SVGInline
         className={classnames('icon-360', { 'icon-360-active': value })}
         classSuffix=""
@@ -27,25 +32,8 @@ const Is360 = React.memo(({
         cleanup={['title']}
         component="div"
       />
+      </HelpIconComponent>
     </div>
-    {showHint
-      && (
-        <div className="hint-360">
-          {'For 360 videos, we recommend using only videos \n downloaded from your computer. \n'}
-          &nbsp;
-          {downloaderEnabled && 'You can use '}
-          {downloaderEnabled && (
-            <a
-              href="http://download.vidcloud.io/"
-              className="library__block--title"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-          our downloader
-            </a>
-          ) }
-        </div>
-      )}
   </div>
 ));
 
