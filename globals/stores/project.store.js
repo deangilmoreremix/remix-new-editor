e6aba5265020e7dc1aff7b67236864a217340d79
himanshu@vidoeremix.io <himanshu@vidoeremix.io>
2023-10-31 11:38:18 +0530

    Added this to process running

--- globals/stores/project.store.js
+++ globals/stores/project.store.js
@@ -1606,7 +1606,7 @@ export default class ProjectStore extends BaseStore {
     console.log(result);
     if(result.isVideoMergeProcess) {
       console.log("call jdjhfgd")
-      setTimeout(processRunning(id), 10000); // Call the function again after 10 seconds
+      setTimeout(this.processRunning(id), 10000); // Call the function again after 10 seconds
     }
   };
 
