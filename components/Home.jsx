d793dfb22f2cf03f6b05435f4e474d1a58493106
himanshu@vidoeremix.io <himanshu@vidoeremix.io>
2023-11-02 13:44:57 +0530

    updated code

--- components/Home.jsx
+++ components/Home.jsx
@@ -276,24 +276,6 @@ const Home = observer(() => {
     }
   }, [isEnabled])
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setProgressMessage('Project is saving...');
-      setProgress((prevProgress) => {
-        if (prevProgress >= 100) {
-          setProgressMessage('Project saved, ready for sharing.')
-          clearInterval(interval); // Stop the progress bar when it reaches 100%
-          return 100;
-        }
-        return prevProgress + 1;
-      });
-    }, 60000); // Increment progress every 1200ms (2 minutes)
-
-    return () => {
-
-      clearInterval(interval); // Clean up the interval on component unmount
-    };
-  }, [progress]);
 
 
   useEffect(() => {
